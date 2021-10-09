651eeaefa8cc09987a8d1a4cb18d74150a824cdf
Kristen McClure <k.x.mcclure@gmail.com>
2021-10-09 15:54:46 -0700

    Update proset.js

--- proset.js
+++ proset.js
@@ -160,19 +160,19 @@ class ProSet {
     */
 
     const intro = `
-  ${chalk.red('P')}${chalk.yellow('R')}${chalk.green('O')}${chalk.cyan('S')}${chalk.blue('E')}${chalk.magenta('T')}: A Game of Matching
+${chalk.red('P')}${chalk.yellow('R')}${chalk.green('O')}${chalk.cyan('S')}${chalk.blue('E')}${chalk.magenta('T')}: A Game of Matching
 
-  A set is any number of cards between 3 and 7 where there
-  are totaled either an even number of a colored dot or none of
-  that color dot.
+A set is any number of cards between 3 and 7 where there
+are totaled either an even number of a colored dot or none of
+that color dot.
 
-  There is always a set in any given draw of seven cards from the deck.
+There is always a set in any given draw of seven cards from the deck.
 
-  To submit a set, note the numbers below each card and submit
-  in a comma separated list. (ex: 1,2,3)
+To submit a set, note the numbers below each card and submit
+in a comma separated list. (ex: 1,2,3)
 
-  If you need help, you can type 'help' at the prompt and it will provide
-  you with all of the sets in the given draw.
+If you need help, you can type 'help' at the prompt and it will provide
+you with all of the sets in the given draw.
     `
     console.log(intro)
     this.colorBlind()
